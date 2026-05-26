45d79035612df7739902741c2aea1d7c63a9188f
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:12:51 +0100

    feat: implement tf-atom-vpc-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,29 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the VPC"
+  value       = try(aws_vpc.this[0].id, null)
+}
+
+output "arn" {
+  description = "ARN of the VPC"
+  value       = try(aws_vpc.this[0].arn, null)
+}
+
+output "cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = try(aws_vpc.this[0].cidr_block, null)
+}
+
+output "default_route_table_id" {
+  description = "ID of the default route table"
+  value       = try(aws_vpc.this[0].default_route_table_id, null)
+}
+
+output "default_security_group_id" {
+  description = "ID of the default security group"
+  value       = try(aws_vpc.this[0].default_security_group_id, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,36 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  validation {
+    condition     = can(cidrhost(var.cidr_block, 0))
+    error_message = "cidr_block must be a valid CIDR notation."
+  }
+}
+
+variable "instance_tenancy" {
+  description = "Tenancy of instances (default, dedicated, host)"
+  type        = string
+  default     = "default"
+  validation {
+    condition     = contains(["default", "dedicated", "host"], var.instance_tenancy)
+    error_message = "instance_tenancy must be default, dedicated, or host."
+  }
+}
+
+variable "enable_dns_support" {
+  description = "Whether DNS resolution is supported"
+  type        = bool
+  default     = true
+}
+
+variable "enable_dns_hostnames" {
+  description = "Whether instances get public DNS hostnames"
+  type        = bool
+  default     = true
+}
+
+variable "assign_generated_ipv6_cidr_block" {
+  description = "Whether to assign an IPv6 CIDR block"
+  type        = bool
+  default     = false
+}
